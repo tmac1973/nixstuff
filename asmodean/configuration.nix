4899b2d212b1eacef92274c4737c16e286ed5423
Tim MacDonald <tbaxmacdonald@gmail.com>
2025-06-02 23:05:04 -0500

    updates

--- asmodean/configuration.nix
+++ asmodean/configuration.nix
@@ -24,7 +24,7 @@
   networking.hostName = "asmodean"; # Define your hostname.
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
+  nix.settings.trusted-users = [ "root" "tim" ];
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
--- homemgr/home.nix
+++ homemgr/home.nix
@@ -20,12 +20,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    jetbrains.pycharm-community-bin
-    go
-    cargo
     vscodium
-    uv
-    poetry
+    devenv
     
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
